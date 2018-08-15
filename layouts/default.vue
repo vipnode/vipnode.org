81ddefa3efb46da92072783eafa15e94064d33ff
Tracy Osborn <tracy@weddinglovely.com>
2018-08-15 17:11:54 -0400

    Set up links between pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="container">
-    <h1 id="vipnode">👑  <a href="/">vipnode</a> 👑</h1>
+    <div class="nav">
+      <h1 id="vipnode"><a href="/">vipnode</a></h1>
+      <h2>Reserve a spot for your Go-Ethereum Light Client, powered by a smart contract.</h2>
+      <nav>
+        <ul>
+          <li>
+            <nuxt-link to="/faq">FAQ</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/status">Status</nuxt-link>
+          </li>
+          <li>
+            <a href="https://medium.com/@shazow/an-economic-incentive-for-running-ethereum-full-nodes-ecc0c9ebe22">Blog</a>
+          </li>
+          <li>
+            <nuxt-link to="/">Reserve</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
 
     <nuxt/>
   </div>
