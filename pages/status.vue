7704185b4e5816ae0c31a2408004a0f1ba22e152
Tracy Osborn <tracy@weddinglovely.com>
2018-08-15 17:03:16 -0400

    Added Status page

---
+++ pages/status.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>Status Dashboard</h1>
+
+    <div>
+      <h3>Number of vipnode hosts active/registered:</h3>
+      <p>????</p>
+    </div>
+    <div>
+      <h3>Number of vipnode clients active/registered:</h3>
+      <p>????</p>
+    </div>
+    <div>
+      <h3>Total client balance available:</h3>
+      <p>????</p>
+    </div>
+    <div>
+      <h3>Total host balance earned:</h3>
+      <p>????</p>
+    </div>
+  </article>
+</template>
