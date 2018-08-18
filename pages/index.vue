ef38b05fff6ca3df954b2877d0ebcfc5382e0372
Tracy Osborn <tracy@weddinglovely.com>
2018-08-18 16:45:33 -0400

    Updated responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -81,27 +81,19 @@
 
     <section id="sponsors" class="text-center">
       <h2>Sponsors</h2>
-      <div class="row">
-        <div class="three columns">
-          <a href="https://www.ethereum.org/foundation">
-            <img src="~/static/img/ethereum.png" alt="Ethereum Foundation" />
-          </a>
-        </div>
-        <div class="three columns">
-          <a href="https://infura.io/">
-            <img src="~/static/img/infura.png" alt="Infura" />
-          </a>
-        </div>
-        <div class="three columns">
-          <a href="">
-            <img src="~/static/img/you.png" alt="You?" />
-          </a>
-        </div>
-        <div class="three columns">
-          <a href="">
-            <img src="~/static/img/you.png" alt="You?" />
-          </a>
-        </div>
+      <div class="grid">
+        <a href="https://www.ethereum.org/foundation">
+          <img src="~/static/img/ethereum.png" alt="Ethereum Foundation" />
+        </a>
+        <a href="https://infura.io/">
+          <img src="~/static/img/infura.png" alt="Infura" />
+        </a>
+        <a href="mailto:andrey@vipnode.org" class="hidden-md-down">
+          <img src="~/static/img/you.png" alt="You?" />
+        </a>
+        <a href="mailto:andrey@vipnode.org" class="hidden-lg-down">
+          <img src="~/static/img/you.png" alt="You?" />
+        </a>
       </div>
     </section>
   </article>
