fb32472bfe181340f9c9e90e871079216b203441
Tracy Osborn <tracy@weddinglovely.com>
2018-08-21 00:25:51 -0400

    Some (hacky) fixes on the positioning of the tooltip

--- components/Contract.vue
+++ components/Contract.vue
@@ -5,8 +5,8 @@
       <li v-for="msg in messages" :class="msg.kind">{{msg.body}}</li>
     </ul>
     <label for="enode">What is your enode id?
-      <div class="tooltip-container" v-on:click="tooltip=!tooltip">
-        <div class="enode-help tooltip" v-if="tooltip">
+      <div class="tooltip-container" v-on:click="enodetooltip=!enodetooltip">
+        <div class="enode-help tooltip" v-if="enodetooltip">
           <p><strong>When you start your Go Ethereum client, it prints out your enode ID.</strong> This encodes the public key of your node that can be used for authentication.</p>
           <p><strong>It looks like this</strong>:<br/>
           <code>enode://6f8a80d143…b39763a4c0@123.123.123.123:30303?discport=30301</code></p>
@@ -17,7 +17,14 @@
       </div>
     </label>
     <input type="text" v-model="enode" placeholder="enode://..." name="enode" class="enode"/>
-    <label for="amount">What would you like to pay for 30 days?</label>
+    <label for="amount">What would you like to pay for 30 days?
+      <div class="tooltip-container" v-on:click="amounttooltip=!amounttooltip">
+        <div class="enode-help tooltip" v-if="amounttooltip" style="transform: translateX(-42%);">
+          <p>Pay what you want while vipnode is in beta &#8212; amount will not affect your vipnode status. You will receive a reserved peer slot on our VIP network, allowing you to bypass the server’s limits even if the server is full.<!--XXX: Update this with better text.--></p>
+          <button type="button" class="button-primary">Got it</button>
+        </div>
+      </div>
+    </label>
     <input type="text" v-model="amount" value="0.2" placeholder="0.2" name="amount" class="amount"/><span> in ETH</span><br/>
     <input type="submit" value="Execute Smart Contract" :disabled="loading" class="button-primary"/>
     <p><small>(requires <a href="https://metamask.io/" target="_blank">a web3-enabled browser</a>)</small></p>
@@ -48,8 +55,8 @@ export default {
       amount: '0.02',
       messages: [],
       pendingTx: '',
-      tooltip: false,
-      hi: true,
+      enodetooltip: false,
+      amounttooltip: false,
     }
   },
   methods: {
