1aacd0c87ba0a4e9d4099562cde59f9fc1d9585f
Tracy Osborn <tracy@weddinglovely.com>
2018-08-29 02:11:18 -0400

    Added a basic Twitter card

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,10 @@ module.exports = {
   head: {
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { property: 'og:type', content: 'website' },
+      { property:'og:image', name: 'twitter:image', content: 'img/preview-card.png'},
+      { name: 'twitter:card', content: 'summary_large_image'}
     ],
     link: [
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=PT+Mono|PT+Sans' },
