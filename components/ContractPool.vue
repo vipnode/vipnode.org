ab1d6871440bd44d40f7a4c958245544a79270fa
Andrey Petrov <andrey.petrov@shazow.net>
2018-10-15 13:32:02 -0400

    port ContractPool to ethers.js

--- components/ContractPool.vue
+++ components/ContractPool.vue
@@ -22,7 +22,7 @@
       </form>
 
       <p>
-        <div class="balance">Balance: <span class="eth">{{fromWei(active.balance, 'ether')}} ETH</span></div>
+        <div class="balance">Balance: <span class="eth">{{formatEther(active.balance)}} ETH</span></div>
         <button v-if="active.balance > 0" :disabled="loading" v-on:click="requestWithdraw" style="width: 172px;">Request Withdraw</button>
       </p>
 
@@ -34,7 +34,7 @@
 
     <div class="messages" v-if="pendingTx">
       <p class="success">
-        Transaction submitted. It can take a few minutes. <a :href="'https://etherscan.io/tx/' + pendingTx" target="_blank">Watch it here.</a>
+        Transaction submitted. It can take a few minutes. <a :href="txURL(pendingTx)" target="_blank">Watch it here.</a>
       </p>
     </div>
 
@@ -42,15 +42,16 @@
 </template>
 
 <script>
+import { providers, utils, Contract, Wallet } from "ethers";
 import Tooltip from "~/components/Tooltip.vue"
 
 const reNodeID = new RegExp('[0-9a-f]{128}');
 
-function getContract() {
+function getContract(provider) {
   //const address = "TODO"; // Mainnet
   const address = "0xb2f8987986259facdc539ac1745f7a0b395972b1"; // Rinkeby
-  const contract = web3.eth.contract([{"constant":true,"inputs":[],"name":"withdrawInterval","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"operator","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"clients","outputs":[{"name":"balance","type":"uint256"},{"name":"timeLocked","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_operator","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"client","type":"address"},{"indexed":false,"name":"timeLocked","type":"uint256"}],"name":"ForceSettle","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"client","type":"address"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Balance","type":"event"},{"constant":true,"inputs":[{"name":"_client","type":"address"},{"name":"_minBalance","type":"uint256"}],"name":"checkBalance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"addBalance","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"forceSettle","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"forceWithdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_client","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_release","type":"bool"}],"name":"opSettle","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_amount","type":"uint256"}],"name":"opWithdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]);
-  return contract.at(address);
+  const abi = [{"constant":true,"inputs":[],"name":"withdrawInterval","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_client","type":"address"},{"name":"_minBalance","type":"uint256"}],"name":"checkBalance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"operator","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"clients","outputs":[{"name":"balance","type":"uint256"},{"name":"timeLocked","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"forceWithdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_client","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_release","type":"bool"}],"name":"opSettle","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_amount","type":"uint256"}],"name":"opWithdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"forceSettle","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"addBalance","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[{"name":"_operator","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"client","type":"address"},{"indexed":false,"name":"timeLocked","type":"uint256"}],"name":"ForceSettle","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"client","type":"address"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Balance","type":"event"}];
+  return new Contract(address, abi, provider);
 }
 
 export default {
@@ -68,16 +69,16 @@ export default {
     }
   },
   methods: {
-    fromWei(n) {
-      return web3.fromWei(n, 'ether').toString();
+    txURL(tx) {
+      return "https://rinkeby.etherscan.io/tx/" + tx;
     },
-    load() {
+    formatEther: utils.formatEther,
+    async load() {
       this.messages = [];
       this.loading = true;
 
+        await this.loadStatus(this.getContract());
       try {
-        this.loadStatus(this.getContract(), function() {
-        }.bind(this));
       } catch(e) {
         this.error('Unexpected error occurred. Check console for more details.', e);
       } finally {
@@ -85,39 +86,43 @@ export default {
       }
       return false; // Prevent submit
     },
-    whitelist() {
+    async whitelist() {
       const enodeParts = this.enode.match(reNodeID);
       if (!enodeParts || enodeParts.length !== 1) {
         return this.error('Invalid enodeID: Should be 128 hex characters.')
       }
       const cleanEnode = enodeParts[0];
-
       const msg = {
         account: this.active.account,
-        authorized: cleanEnode,
+        authorizeNode: cleanEnode,
         timestamp: +new Date(),
       }
-      web3.eth.sign(this.active.account, JSON.stringify(msg), function (err, result) {
-        console.log(err, result);
-      });
+      const signer = this.provider.getSigner(this.active.account);
+      try {
+        const signedMsg = await signer.signMessage(JSON.stringify(msg));
+        console.log(signedMsg);
+        // XXX: JSONRPC to pool whitelist
+      } catch(err) {
+        return this.error('Failed to sign message.', err);
+      }
+
     },
     requestWithdraw() {
       this.error("request withdraw: not implemented yet");
     },
-    addBalance() {
-      const weiAmount = web3.toWei(this.amount, 'ether');
+    async addBalance() {
+      const weiAmount = utils.parseUnits(this.amount, 'ether');
       const contract = this.getContract();
       if (!contract) return;
 
-      contract.addBalance.sendTransaction({value: weiAmount}, function(err, res) {
-        if (err) {
-          if (err.message && err.message.indexOf('User denied transaction signature.') !== -1)  {
-            return this.warning('Transaction aborted. Try again?')
-          }
-          return this.error('Smart contract transaction failed.', err);
+      try {
+        this.pendingTx = await contract.addBalance.sendTransaction({value: weiAmount});
+      } catch(err) {
+        if (err.message && err.message.indexOf('User denied transaction signature.') !== -1)  {
+          return this.warning('Transaction aborted. Try again?')
         }
-        this.pendingTx = res;
-      }.bind(this));
+        return this.error('Smart contract transaction failed.', err);
+      }
     },
     error(msg, exc) {
       this.messages.push({body: msg, kind: 'error'});
@@ -137,52 +142,53 @@ export default {
     getContract() {
       if (this.contract) return this.contract;
       try {
-        this.contract = getContract();
+        this.contract = getContract(this.provider);
         return this.contract;
-      } catch(e) {
-        this.error('Failed to load smart contract. Make sure you have a web3-enabled browser.', e);
+      } catch(err) {
+        this.error('Failed to load smart contract. Make sure you have a web3-enabled browser.', err);
         return;
       }
     },
-    loadStatus(contract, cb) {
+    async loadStatus(contract) {
       if (!contract) return;
       if (web3 === undefined) {
         return this.error("Please use a web3-enabled browser to execute this smart contract.");
       }
 
-      web3.version.getNetwork(function(err, netId) {
-        if (netId !== this.networkID) {
-          return this.error("Please switch your wallet to "+ this.networkName +".");
-        }
+      const network = await this.provider.getNetwork();
+      if (String(network.chainId) !== this.networkID) {
+        return this.error("Please switch your wallet from "+ network.name +" to "+ this.networkName +".");
+      }
 
-        web3.eth.getAccounts(function(err, res) {
-          if (res.length === 0) {
-            return this.error("Please unlock your wallet first.");
-          }
-          this.accounts = res;
-          this.active = {
-            account: res[0],
-            balance: 0,
-          };
+      const accounts = await this.provider.listAccounts();
+      if (accounts.length === 0) {
+        return this.error("Please unlock your wallet first.");
+      }
+      this.accounts = accounts;
+      this.active = {
+        account: accounts[0],
+        balance: 0,
+      };
 
-          contract.clients.call(this.active.account, function(err, res) {
-            if (err) {
-              return this.error('Failed to load client balance.', err);
-            }
+      let client;
+      try {
+        client = await contract.clients(this.active.account)
+      }catch (err) {
+        return this.error('Failed to load client balance.', err);
+      }
 
-            this.active.balance = res[0];
-            this.active.timelocked = res[1];
-          }.bind(this));
-
-          // So deep.
-          cb();
-        }.bind(this));
-      }.bind(this));
+      this.active.balance = client[0];
+      this.active.timelocked = client[1];
     },
   },
   mounted() {
     this.load();
     this.enode = this.$route.query.enode;
+  },
+  computed: {
+    provider() {
+      return new providers.Web3Provider(web3.currentProvider);
+    },
   },
   components: { Tooltip },
 }
