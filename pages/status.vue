00c071735764eb7c664bbd4fc257e8745b492a5b
Andrey Petrov <andrey.petrov@shazow.net>
2018-11-28 12:41:34 -0800

    Add /status dashboard stub

--- pages/status.vue
+++ pages/status.vue
@@ -2,7 +2,11 @@
   <article>
     <h1>Status Dashboard</h1>
 
-    <div id="status-dashboard-grid">
+    <ul class="messages" v-if="messages.length > 0">
+      <li v-for="msg in messages" :class="msg.kind">{{msg.body}}</li>
+    </ul>
+
+    <div id="status-dashboard">
       <div>
         <h3>Number of vipnode hosts active/registered:</h3>
         <p>Coming soon</p>
@@ -20,15 +24,41 @@
         <p>Coming soon</p>
       </div>
     </div>
+
+    <pre>{{dump}}</pre>
   </article>
 </template>
 
 <script>
-import Sponsors from "~/components/Sponsors.vue"
-import Newsletter from "~/components/Newsletter.vue"
+import Pool from "~/lib/poolrpc.js";
 
 export default {
-  components: { Sponsors, Newsletter },
+  name: "Status",
+  data() {
+    return {
+      messages: [],
+      dump: {
+        loading: true,
+      },
+    };
+  },
+  methods: {
+    error(msg, exc) {
+      this.messages.push({body: msg, kind: 'error'});
+      if (exc !== undefined) console.error(exc);
+    },
+    async load() {
+      try {
+        let r = await Pool.RPC("pool_status");
+        this.dump = r;
+      } catch(err) {
+        return this.error(err.message, err);
+      }
+    },
+  },
+  mounted() {
+    this.load();
+  },
   head() {
     return {
       title: "Status",
